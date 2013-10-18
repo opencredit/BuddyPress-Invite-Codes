04b47496664950ab9e21d2a23b5c538349afd9d4
Scott Kingsley Clark <lol@scottkclark.com>
2013-10-18 15:32:47 -0500

    Update JS for Coding Standards; Implement error message; Fix document.location usage

--- js/bp-invite-codes.js
+++ js/bp-invite-codes.js
@@ -1,63 +1,75 @@
-jQuery(document).ready(function($) {
+jQuery( function( $ ) {
 
 	// Listen for clicks to the join button
-	$('body').on( 'click', '.class_bp_invite_codes', function() {
+	$( 'body' ).on( 'click', '.class_bp_invite_codes', function() {
 
 		// Prompt for invite code
 		var invite_code = prompt( bp_invite_codes.prompt );
 
 		// If no code provided, bail
-		if ( ! invite_code )
+		if ( !invite_code ) {
 			return;
+		}
 
-		// Setup our vairiables
-		var gid     = $(this).parent().attr('id');
-		var thelink = $(this);
-		gid         = gid.split('-');
-		gid         = gid[1];
+		// Setup our variables
+		var $this = $( this ),
+			gid = $this.parent().prop( 'id' );
+
+		gid = gid.split( '-' )[ 1 ];
 
 		// Run our ajax request
-		$.ajax({
-			url: bp_invite_codes.ajaxurl,
-			data: {
-				'action': 'bp_invite_codes_bp_get_group_join_button',
-				'entered_code': invite_code,
-				'group_id': gid
+		$.ajax( {
+			url : bp_invite_codes.ajaxurl,
+			data : {
+				'action' : 'bp_invite_codes_bp_get_group_join_button',
+				'entered_code' : invite_code,
+				'group_id' : gid
 			},
-			dataType: 'json',
-			success: function( response ) {
+			dataType : 'json',
+			success : function( response ) {
+
 				if ( 'join' == response.data ) {
 
-					var nonce = $('#link_href_'+gid).val()
-					nonce = nonce.split('?_wpnonce=');
-					nonce = nonce[1].split('&');
+					var nonce = $( '#link_href_' + gid ).val()
+					nonce = nonce.split( '?_wpnonce=' );
+					nonce = nonce[1].split( '&' );
 					nonce = nonce[0];
 
-					$.post( bp_invite_codes.ajaxurl, {
-						action: 'joinleave_group',
-						'cookie': encodeURIComponent(document.cookie),
-						'gid': gid,
-						'_wpnonce': nonce
-					},
-					function( response )
-					{
-						var parentdiv = thelink.parent();
-						if ( ! $('body.directory').length )
-							location.href = location.href;
-						else {
-							$(parentdiv).fadeOut(200,
-								function() {
-									parentdiv.fadeIn(200).html(response);
-								}
-								);
-						}
-					});
-					return false;
+					$.post(
+						bp_invite_codes.ajaxurl,
+						{
+							action : 'joinleave_group',
+							'cookie' : encodeURIComponent( document.cookie ),
+							'gid' : gid,
+							'_wpnonce' : nonce
+						},
+						function( response ) {
 
-				} else {
-					alert(response.data);
+							var parentdiv = $this.parent();
+
+							if ( !$( 'body.directory' ).length ) {
+								document.location = document.location.href;
+							}
+							else {
+								$( parentdiv ).fadeOut( 200, function () {
+									parentdiv.fadeIn( 200 ).html( response );
+								} );
+							}
+
+						}
+					);
+
 				}
+				else {
+					alert( response.data );
+				}
+
+			},
+			error : function() {
+
+				alert( 'There was an issue requesting membership, please contact your site administrator' );
+
 			}
-		});
-	});
-});
+		} );
+	} );
+} );
